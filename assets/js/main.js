019d2a4501c83a7afef2a86cb154413ab0e3ce68
Doan Le Thanh\Citigo <thanh.dl@citigo.com.vn>
2018-12-19 10:21:29 +0700

    z

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,9 @@ $(function() {
             $('.header-home').addClass('header-transparent');
         }
     })
+    $('ul.nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).fadeIn(300);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).fadeOut(300);
+    });
 });
